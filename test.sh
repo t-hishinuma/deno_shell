8d93fb2fa4e1fd2c898f42d74996b7f8ad159336
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2023-10-15 01:09:50 +0900

    ugoita

--- test.sh
+++ test.sh
@@ -3,14 +3,16 @@
 /*
 //
 if ! command -v deno > /dev/null ; then
-    echo hello
+    echo install Deno!
+    curl -fsSL https://deno.land/x/install/install.sh | sh
+    export DENO_INSTALL="$HOME/.deno"
+    export PATH="$DENO_INSTALL/bin:$PATH"
 else
     echo Deno is already installed
 fi
-
-exec deno run --allow-all "$0"
+    exec deno run --allow-all "$0"
 */
 
-# start Deno script
+// start Deno script
 import { exec } from "https://deno.land/x/exec/mod.ts";
 await exec('echo "Deno!!"');
